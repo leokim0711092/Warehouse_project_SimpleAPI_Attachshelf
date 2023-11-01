8c4e2207b2eaaaed1894689c2b1c7cf592a98880
leokim0711092 <leokim10073@gmail.com>
2023-11-01 04:24:43 +0000

    Add attach_shelf pkg

---
+++ attach_shelf/src/approach_service_server.cpp
@@ -0,0 +1,212 @@
+#include "rclcpp/node.hpp"
+#include "rclcpp/rate.hpp"
+#include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
+#include <cmath>
+#include <cstddef>
+#include <math.h>
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include "std_msgs/msg/detail/empty__struct.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "std_msgs/msg/empty.hpp"
+#include "custom_interfaces/srv/go_to_loading.hpp"
+
+class Approach_Server : public rclcpp::Node{
+    
+    public:
+        Approach_Server(): Node("approach_server"), tf_buffer_(this->get_clock()), tf_listener_(tf_buffer_){
+            
+            callback_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+
+            rclcpp::SubscriptionOptions subscription_options;
+            subscription_options.callback_group = callback_group_;
+
+            sub_scan = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10 , 
+            std::bind(&Approach_Server::scan_callback, this, std::placeholders::_1), subscription_options);
+
+            srv_ = this->create_service<custom_interfaces::srv::GoToLoading>("/approach_shelf",
+            std::bind(&Approach_Server::attach_callback, this, std::placeholders::_1, std::placeholders::_2),rmw_qos_profile_services_default,
+            callback_group_);
+
+            pub_ = this->create_publisher<geometry_msgs::msg::Twist>("robot/cmd_vel", 10);
+            pub_elevator = this->create_publisher<std_msgs::msg::Empty>("/elevator_up", 10);
+
+        }
+    private:
+        
+        geometry_msgs::msg::Twist vel;
+        std_msgs::msg::Empty ele;
+
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub_scan;
+        rclcpp::Service<custom_interfaces::srv::GoToLoading>::SharedPtr srv_;
+        tf2_ros::TransformBroadcaster broadcaster_{this}; //this will disappear in short time
+        // tf2_ros::StaticTransformBroadcaster static_broadcaster_{this};
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
+        rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr pub_elevator;
+        rclcpp::CallbackGroup::SharedPtr callback_group_;
+
+        tf2_ros::Buffer tf_buffer_;
+        tf2_ros::TransformListener tf_listener_;
+
+        float x1, x2, y1, y2;
+        size_t accept_idx_size=0;
+
+        bool ud_date_cart_frame = false;
+
+        void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
+            
+            // RCLCPP_INFO(this->get_logger(), "Intensity size:%zu" ,msg->intensities.size());
+            // for(size_t i=0; i<msg->intensities.size();i++)
+            // if(msg->intensities[i]>= 8000)
+            // RCLCPP_INFO(this->get_logger(), "Scan intensity:%zu, %f",i, msg->intensities[i]);
+
+            int minimum_idx_difference = 30;
+            std::vector<std::pair<float, int>> intensity_cont;
+
+            for(size_t i=0; i<msg->intensities.size();i++) 
+            intensity_cont.emplace_back(msg->intensities[i],i);
+
+            std::sort(intensity_cont.begin(), intensity_cont.end(),
+                  [](const std::pair<float, int>& a, const std::pair<float, int>& b)-> 
+                  bool {
+                    return a.first > b.first;
+                  });
+            std::vector<int> accept_idx;
+            for(const auto & pair : intensity_cont){
+                
+
+                bool close = false;
+                for(const int acp : accept_idx){
+                    if(std::abs(acp - pair.second) < minimum_idx_difference){
+                        close = true;
+                        break;
+                    }
+                }
+
+                if(!close) accept_idx.push_back(pair.second);
+                if(accept_idx.size() == 2) break;
+            }
+            accept_idx_size = accept_idx.size();
+
+            int idx_1 = accept_idx[0];
+            int idx_2 = accept_idx[1];
+            
+            float angle1 = msg->angle_min + idx_1*msg->angle_increment;
+            float angle2 = msg->angle_min + idx_2*msg->angle_increment;
+
+            x1 = msg->ranges[idx_1]* cos(angle1);
+            y1 = msg->ranges[idx_1]* sin(angle1);
+
+            x2 = msg->ranges[idx_2]* cos(angle2);
+            y2 = msg->ranges[idx_2]* sin(angle2);
+            
+            if (ud_date_cart_frame ) {
+                broadcaster_.sendTransform(broadcast_transform( (x1+x2)/2 + std::fabs((y1-y2)/2) , (y1+y2)/2));
+            }
+
+        }
+
+
+        void attach_callback(const std::shared_ptr<custom_interfaces::srv::GoToLoading::Request> req, 
+        const std::shared_ptr<custom_interfaces::srv::GoToLoading::Response> res){
+            rclcpp::Rate l(1);
+
+            if(req->attach_to_shelf == true && accept_idx_size == 2){
+                ud_date_cart_frame = true;
+                broadcaster_.sendTransform(broadcast_transform( (x1+x2)/2 + std::fabs((y1-y2)/2) , (y1+y2)/2));
+                l.sleep();
+                move_towards_cart_frame();
+                RCLCPP_INFO(this->get_logger(), "Approach call");
+
+                RCLCPP_INFO(this->get_logger(), "Accept_idz = %zu",accept_idx_size);
+
+                res->complete = true;
+            }else if(req->attach_to_shelf == false && accept_idx_size == 2){
+                ud_date_cart_frame = true;
+                broadcaster_.sendTransform(broadcast_transform( (x1+x2)/2 + std::fabs((y1-y2)/2) , (y1+y2)/2));
+                l.sleep();
+                vel.linear.x = 0;
+                vel.angular.z = 0;
+                pub_->publish(vel);
+                RCLCPP_INFO(this->get_logger(), "Approach not call but publish frame");
+                res->complete = false;
+            }else {
+                RCLCPP_INFO(this->get_logger(), "Approach did not meet two legs");
+                res->complete = false;
+            }
+        }
+
+        geometry_msgs::msg::TransformStamped broadcast_transform(float x,float y){
+                
+                geometry_msgs::msg::TransformStamped t;
+                t.header.stamp = this->now();
+                t.header.frame_id = "robot_front_laser_base_link";
+                t.child_frame_id = "cart_frame";
+                t.transform.translation.x = x;
+                t.transform.translation.y = y;
+                t.transform.translation.z = 0.0;  // Assuming it's 2D plane
+                t.transform.rotation.x = 0.0;
+                t.transform.rotation.y = 0.0;
+                t.transform.rotation.z = 0.0;
+                t.transform.rotation.w = 1.0;
+
+                return t;
+        }
+
+         void move_towards_cart_frame(){
+            // Get the transform from base_link to cart_frame
+            geometry_msgs::msg::TransformStamped transform;
+            
+            try
+            {
+                transform = tf_buffer_.lookupTransform("robot_front_laser_base_link", "cart_frame", rclcpp::Time(0));
+            }
+            catch (tf2::TransformException &ex)
+            {
+                RCLCPP_ERROR(this->get_logger(), "%s", ex.what());
+                return;
+            }
+
+            // Compute the direction and magnitude based on the transform's translation
+            float distance_to_point = std::sqrt(
+                transform.transform.translation.x * transform.transform.translation.x +
+                transform.transform.translation.y * transform.transform.translation.y);
+
+            float desired_distance = distance_to_point;  // Approach by 30cm
+            vel.linear.x = desired_distance*0.5; // move in x-direction of robot's frame
+            RCLCPP_INFO(this->get_logger(), "vel.linear = %f",vel.linear.x);
+
+            pub_->publish(vel);
+
+            
+            rclcpp::Rate l(0.4);
+            l.sleep();
+            ud_date_cart_frame = false;
+            vel.linear.x = 0;
+            pub_->publish(vel);
+            pub_elevator->publish(ele);
+    }
+
+
+};
+
+int main(int argc, char *argv[]){
+    rclcpp::init(argc, argv);
+
+    rclcpp::executors::MultiThreadedExecutor executor;
+    auto node = std::make_shared<Approach_Server>();
+    executor.add_node(node);
+    executor.spin();
+
+
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ attach_shelf/src/pre_approach_v2.cpp
@@ -0,0 +1,148 @@
+#include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "nav_msgs/msg/detail/odometry__struct.hpp"
+#include "rclcpp/executors.hpp"
+#include "rclcpp/logging.hpp"
+#include "rclcpp/node.hpp"
+#include "rclcpp/publisher.hpp"
+#include "rclcpp/subscription.hpp"
+#include "rclcpp/utilities.hpp"
+#include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
+#include <cmath>
+#include <functional>
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <string>
+#include "custom_interfaces/srv/go_to_loading.hpp"
+
+
+class Attach_self : public rclcpp::Node{
+
+    public:
+        Attach_self(int &argc, char **argv): Node("attach_self"){
+            
+            ob = argv[2];
+            de = argv[4];
+            fa = argv[6];
+
+            getting_params();
+
+            sub_scan = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10 , 
+            std::bind(&Attach_self::scan_callback, this, std::placeholders::_1));
+
+            sub_odom = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, 
+            std::bind(&Attach_self::odom_callback, this, std::placeholders::_1));
+
+            pub_ = this->create_publisher<geometry_msgs::msg::Twist>("robot/cmd_vel", 10);
+
+            client = this->create_client<custom_interfaces::srv::GoToLoading>(
+            "/approach_shelf");      
+
+            while (!client->wait_for_service(std::chrono::seconds(1))) {
+                if (!rclcpp::ok()) {
+                    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                    "Interrupted while waiting for the service. Exiting.");
+                    return;
+                }
+                    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                    "service not available, waiting again...");
+            }
+        }
+
+    
+
+    private:
+        float obstacle;
+        int degrees;
+        bool final_approach;
+        bool send = true;   
+        std::string ob;
+        std::string de;
+        std::string fa;
+        rclcpp::Client<custom_interfaces::srv::GoToLoading>::SharedPtr client;
+        
+        geometry_msgs::msg::Twist vel;
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub_scan;
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom;
+        bool turn;  
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
+
+        void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
+            // RCLCPP_INFO(this->get_logger(), "obstacle: %f", obstacle);
+            // if (!final_approach) {
+            //     RCLCPP_INFO(this->get_logger(), "final_approach: false");
+            // }else {
+            //     RCLCPP_INFO(this->get_logger(), "final_approach: true");
+            // }
+            // RCLCPP_INFO(this->get_logger(), "degrees: %i", degrees );
+            
+            if(msg->ranges[359] > obstacle && !turn && send ){
+                vel.linear.x = 0.5;
+            }else if (msg->ranges[359] < obstacle && send){
+                vel.linear.x = 0;
+                turn = true;
+            }
+
+            // RCLCPP_INFO(this->get_logger(), "vel.linear: %f", vel.linear.x);
+
+        }
+        
+
+        void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
+            float cur_degree = euler_degree_transform(msg)/M_PI*180;
+            // RCLCPP_INFO(this->get_logger(), "cur_degrees: %f", cur_degree );
+
+            if (std::fabs(degrees - cur_degree) > 0.5 && turn && send) {
+                vel.angular.z = (degrees - cur_degree)/std::fabs(degrees - cur_degree)*0.4;
+                pub_->publish(vel);
+            }else if(std::fabs(degrees - cur_degree) < 0.5 && turn && send){
+                auto request = std::make_shared<custom_interfaces::srv::GoToLoading::Request>();
+                vel.angular.z = 0;
+                pub_->publish(vel);
+                request->attach_to_shelf = final_approach;
+                client->async_send_request(request, std::bind(&Attach_self::service_response, this, std::placeholders::_1));
+                send = false;
+            }else if(send){
+                vel.angular.z = 0;
+                pub_->publish(vel);
+            }
+        }
+
+        void getting_params(){
+            obstacle = std::stof(ob);
+            degrees = std::stoi(de);
+            final_approach = fa == "false" ? false : true;
+        }
+
+        void service_response(rclcpp::Client<custom_interfaces::srv::GoToLoading>::SharedFuture fut){
+            auto status = fut.wait_for(std::chrono::seconds(1));
+            if(status == std::future_status::ready){
+                auto response = fut.get();
+                RCLCPP_INFO(this->get_logger(), "Service was called");
+                std::string fb = response->complete ? "true" : "false";
+                RCLCPP_INFO(this->get_logger(), "Recevied final_approach: %s", fb.c_str());
+            }
+            else {
+                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service");
+            }
+        }
+
+        float euler_degree_transform(const nav_msgs::msg::Odometry::SharedPtr msg){
+                float x = msg->pose.pose.orientation.x;
+                float y = msg->pose.pose.orientation.y;
+                float z = msg->pose.pose.orientation.z;
+                float w = msg->pose.pose.orientation.w; 
+
+            return atan2(2 * (w * z + x * y),1 - 2 * (y * y + z * z));
+        }
+
+};
+
+int main(int argc, char *argv[]){
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Attach_self>(argc, argv));
+    rclcpp::shutdown();
+    return 0;
+}
